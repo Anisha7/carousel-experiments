d3906a612ff04c3c70e4c2df3e3e51e2714e6b25
Anisha Jain <anishajain@outlook.com>
2020-06-12 01:17:47 -0700

    cite

--- src/components/Popup/index.js
+++ src/components/Popup/index.js
@@ -7,8 +7,8 @@ import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
-
-const FormDialog = () => {
+// https://material-ui.com/components/dialogs/#FormDialog.js
+const Popup = () => {
   const [open, setOpen] = useState(true);
 
   const handleClickOpen = () => {
@@ -57,4 +57,4 @@ const FormDialog = () => {
   );
 };
 
-export default FormDialog;
+export default Popup;
